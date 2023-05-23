7d611e2869e835a050195a485202acfa87c68a65
Vasanta Kumar <vasantakmr6@gmail.com>
2023-05-23 10:37:51 +0530

    Day 5

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Main.js is linked")
+
+document.getElementsByTagName("div")[0].innerHTML = "<h1> hello</h1>"
+document.querySelector(".demo").textContent = "<h1> hello</h1>"
+document.querySelectorAll(".demo")[0].textContent = "<h1> hello</h1>"
+document.getElementsByClassName("demo")[0].textContent = "<h1> hello</h1>"
+document.getElementById("hello").innerHTML = "<h1> hello</h1>"
